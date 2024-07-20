700c38758044f0e130100e334cf0460f71124b01
Ritesh dhote <dhoteritesh66@gmail.com>
2024-07-20 14:11:38 +0530

    add file

--- DS-2/data.js
+++ DS-2/data.js
@@ -10,17 +10,41 @@ const fs = require("fs")
                 'Content-Type':'text/html'
             })
             data.forEach((el,i)=>{
-                res.write(`<a href="${el}">${el}</a>`)
+                res.write(`<a href=${el}> <li>${el}</li></a>`)
             })
             res.end()
 
         }else if(req.url != "/favicon.ico"){
-            console.log("req.url in else if", req.url) 
-            let data = fs.readFileSync(`.${req.url}`,"utf-8")
-            res.end(data)
+            let path = fs.existsSync(`.${req.url}`)
+            console.log("path", path)
+            
+            if(path){
+                let check = fs.statSync(`.${req.url}`).isDirectory()
+            //console.log("this is from else", req.url,check)
+            if(check==false){
+                let data = fs.readFileSync(`.${req.url}`,"utf-8")
+                res.end(data)
+            }else if(check==true){
+                let data = fs.readdirSync(`.${req.url}`,"utf-8")
+            res.writeHead(200,{
+                'Content-Type':'text/html'
+            })
+            data.forEach((el,i)=>{
+                    res.write(`<a href= ${req.url} /${el}><li> ${el}</li></a>`)
+                
+                
+            })
+            res.end()
+            }
+
+            }else{
+                res.end("Not a velid path")
+            }
+            
+            
         }
     })
 
-    server.listen(8181,()=>{
+    server.listen(8587,()=>{
         console.log("server statet")
     })
